14af938041af7b7611ab4b2a928a0e867a2ad525
E=MC Sqirreled <emcsquirreled@gmail.com>
2012-09-11 19:53:59 -0700

    OK LED blinks

--- kernel/source/main.s
+++ kernel/source/main.s
@@ -11,11 +11,37 @@ mov r1,#1
 lsl r1,#18
 str r1,[r0,#4]
 
+
+
+/* Main Code */
+
+blink:
+
 /* Turn the OK LED on */
 mov r1,#1
 lsl r1,#16
 str r1,[r0,#40]
 
-/* Loop forever */
-loop$:
-b loop$
+push {r0, r1}
+mov r0,#0x3F0000
+wait1:
+sub r0,#1
+cmp r0,#0
+bne wait1
+pop {r0, r1}
+
+/* Turn the OK LED off */
+mov r1,#1
+lsl r1,#16
+str r1,[r0,#28]
+
+push {r0, r1}
+mov r0,#0x3F0000
+wait2:
+sub r0,#1
+cmp r0,#0
+bne wait2
+pop {r0, r1}
+
+/* Go back to the beginning */
+b blink
